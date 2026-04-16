07bdc6b9f2ee02780f57f22c1377ed666a448cba
hannahdadd <hannahbdadd@googlemail.com>
2026-04-16 12:25:48 +0100

    module 6 03

--- module-06-async/exercises/03-fetch-combine/starter/fetch-combine.js
+++ module-06-async/exercises/03-fetch-combine/starter/fetch-combine.js
@@ -6,10 +6,27 @@
  */
 export async function fetchUserSummaries(fetchFn) {
   void fetchFn;
-  // TODO:
-  // 1. Fetch /users and /posts in parallel with Promise.all
-  // 2. Parse both JSON responses
-  // 3. Count posts per userId
-  // 4. Return [{ id, name, email, postCount }] sorted by id
-  return [];
+
+  // fetch endpoint
+  const [usersRes, postsRes] = await Promise.all([
+    fetchFn('https://jsonplaceholder.typicode.com/users'),
+    fetchFn('https://jsonplaceholder.typicode.com/posts'),
+  ]);
+  const users = await usersRes.json();
+  const posts = await postsRes.json();
+
+  // count posts by user
+  const postCounts = new Map();
+  for (const post of posts) {
+    postCounts.set(post.userId, (postCounts.get(post.userId) ?? 0) + 1);
+  }
+
+  // return data as required
+  return users.map(user => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    postCount: postCounts.get(user.id) ?? 0
+  }))
+  .sort((a, b) => a.id - b.id);
 }
