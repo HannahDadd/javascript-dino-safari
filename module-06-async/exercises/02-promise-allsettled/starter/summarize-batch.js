d6069e114dffed0f40e23923aa9ad99059c47f31
hannahdadd <hannahbdadd@googlemail.com>
2026-04-16 12:48:06 +0100

    module 6 02

--- module-06-async/exercises/02-promise-allsettled/starter/summarize-batch.js
+++ module-06-async/exercises/02-promise-allsettled/starter/summarize-batch.js
@@ -3,6 +3,13 @@
  */
 export async function summarizeSensorBatch(_sensorFns) {
   void _sensorFns;
-  // TODO
-  return [];
+
+  const allResults = await Promise.allSettled(_sensorFns.map(fn => fn()));
+  return allResults.map(result => {
+    if (result.status === 'fulfilled') {
+      return { status: 'fulfilled', value: result.value };
+    } else {
+      return { status: 'rejected', reason: result.reason.message };
+    }
+  });
 }
